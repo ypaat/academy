a21b3b4f27c6dcc57b0c243c3d59a5a5a3bcd4ff
ypaat <hmoorthattil@gmail.com>
2025-02-25 19:41:53 +0530

    Update routes.ts

--- server/routes.ts
+++ server/routes.ts
@@ -58,7 +58,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
   return () => {
     newSocket.close();
   };
-  }, [classId, toast]);
+  
 
   const updateMoveHistory = (currentGame: Chess) => {
   const history = currentGame.history();
